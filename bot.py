074cf14c92f5b6f369db79a513bcb63fc31b69f8
Pordrack <50073154+Pordrack@users.noreply.github.com>
2024-07-08 15:17:51 +0200

    [feat] Using json as a database

--- bot.py
+++ bot.py
@@ -8,10 +8,21 @@ client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 settingsFile= open('settings.json')
 settings=json.load(settingsFile)
+dataFile=open('data.json', 'r', encoding='utf-8')
+data=json.load(dataFile)
+dataFile.close()
+
 
 @client.event
 async def on_ready():
     await tree.sync()
     print("Ready!")
+    data["test"]="Everything fine"
+    save_data()
+
+def save_data():
+    file=open('data.json', 'w', encoding='utf-8')
+    json.dump(data,file)
+    file.close()
 
 client.run(settings["Token"])
