bcfe3141fb44d9ad4e39a905f9513977d57e0937
Pordrack <50073154+Pordrack@users.noreply.github.com>
2024-07-08 17:29:17 +0200

    Moved from a json as a db to a real SQLite3 db

--- bot.py
+++ bot.py
@@ -2,27 +2,69 @@
 import discord
 import json
 from discord import app_commands
+import sqlite3
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 settingsFile= open('settings.json')
 settings=json.load(settingsFile)
-dataFile=open('data.json', 'r', encoding='utf-8')
-data=json.load(dataFile)
-dataFile.close()
+con = sqlite3.connect("data.db")
+
+#region Database management
+def create_tables():
+    sql_statements = [ 
+        """CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY,
+                discordName  TEXT NOT NULL
+        );""",
+        """CREATE TABLE IF NOT EXISTS skills (
+                id INTEGER PRIMARY KEY,
+                skill_name  TEXT DEFAULT "" NOT NULL,
+                skill_desc  TEXT DEFAULT "" NOT NULL,
+                spe1 INTEGER,
+                spe2 INTEGER,
+                spe3 INTEGER
+        );""",
+        """CREATE TABLE IF NOT EXISTS cards (
+                id INTEGER PRIMARY KEY,
+                card_name TEXT DEFAULT "" NOT NULL,
+                cp_name TEXT DEFAULT "" NOT NULL, 
+                owner_name TEXT DEFAULT "" NOT NULL,
+                owner_cohort TEXT DEFAULT "" NOT NULL, 
+                card_description TEXT DEFAULT "" NOT NULL, 
+                bottom_text TEXT DEFAULT "" NOT NULL,
+                cp_value INTEGER DEFAULT 0 NOT NULL,
+                card_background INTEGER DEFAULT 0 NOT NULL,
+                card_watermark INTEGER DEFAULT 0 NOT NULL,
+                skill1_id INTEGER NOT NULL,
+                skill2_id INTEGER NOT NULL,
+                owner_id INTEGER NOT NULL,
+                FOREIGN KEY(skill1_id) REFERENCES skills(id),
+                FOREIGN KEY(skill2_id) REFERENCES skills(id),
+                FOREIGN KEY(owner_id) REFERENCES users(id) ON DELETE CASCADE
+        );""",
+        #A whitelist entry means that a user can modify a card, even if he's not the admin
+        """CREATE TABLE IF NOT EXISTS whitelist_entry (
+                user_id INTEGER,
+                card_id INTEGER,
+                PRIMARY KEY(user_id,card_id),
+                FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
+                FOREIGN KEY(card_id) REFERENCES cards(id) ON DELETE CASCADE
+        );"""
+        ]
+    cur=con.cursor()
+    for statement in sql_statements:
+        cur.execute(statement)
 
 
+create_tables()
+#endregion
+
+#region Bot management
 @client.event
 async def on_ready():
     await tree.sync()
-    print("Ready!")
-    data["test"]="Everything fine"
-    save_data()
-
-def save_data():
-    file=open('data.json', 'w', encoding='utf-8')
-    json.dump(data,file)
-    file.close()
 
 client.run(settings["Token"])
+#endregion
